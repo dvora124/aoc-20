91330a1ee75aed1a03a32cb6ae9269b25ff39b0e
Michal Dvořák <dvorak.michal.cb@seznam.cz>
2020-12-06 14:03:28 +0100

    Puzzle 01 - 2/2 solution

---
+++ 01/01_add2.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+#define DATA_MAX 200
+
+/*-----------------------------------------------------------------------------------------------*/
+/**
+ * Find two numbers which add up to *val* and return their product. If there are multiple such
+ * pairs, the first one is returned.
+ * @param[in]  data    list of numbers
+ * @param[in]  nr      length
+ * @param[in]  val     value the pair ought to add up to
+ * @param[out] mulRes  product of the pair
+ * @return 0 - not found, 1 - found and inscribed to *mulRes*
+ */
+int findNum2 ( int data[], int nr, int val, int * mulRes )
+{
+  int i, j, seek, isFound;
+
+  for ( i = 0; i < nr - 1; i ++ )
+  {
+    seek = val - data[i];
+
+    isFound = 0;
+    for ( j = i + 1; j < nr; j ++ )
+      if ( data[j] == seek )
+      {
+        isFound = 1;
+        break;
+      }
+
+    if ( isFound )
+    {
+     *mulRes = seek * data[i];
+      return 1;
+    }
+  }
+  return 0;
+}
+/*-----------------------------------------------------------------------------------------------*/
+/**
+ * Find three numbers which add up to *val* and return their product. If there are multiple such
+ * tuples, the first one is returned.
+ * @param[in]  data    list of numbers
+ * @param[in]  nr      length
+ * @param[in]  val     value the pair ought to add up to
+ * @param[out] mulRes  product of the pair
+ * @return 0 - not found, 1 - found and inscribed to *mulRes*
+ */
+int findNum ( int data[], int nr, int val, int * mulRes )
+{
+  int i, seek;
+
+  for ( i = 0; i < nr - 2; i ++ )
+  {
+    seek = val - data[i];
+    if ( findNum2 ( data + i, nr - i, seek, mulRes ) )
+    {
+     *mulRes *= data[i];
+      return 1;
+    }
+  }
+  return 0;
+}
+/*-----------------------------------------------------------------------------------------------*/
+int main ( int argc, char * argv [] )
+{
+  int i, data[DATA_MAX], mulRes;
+
+  printf ( "Enter values (200):\n" );
+  for ( i = 0; i < DATA_MAX; i ++ )
+    if ( scanf ( "%d", data + i ) != 1 )
+    {
+      printf ( "Invalid input.\n" );
+      return 1;
+    }
+
+  if ( ! findNum ( data, DATA_MAX, 2020, &mulRes ) )
+  {
+    printf ( "No such tuple of numbers found\n" );
+    return 0;
+  }
+  printf ( "The product is: %d\n", mulRes );
+  return 0;
+}
